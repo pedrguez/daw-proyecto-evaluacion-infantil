c613c4394c7db1c79e87251f2f398319e77361c3
pedrguez <pedrguezluis@gmail.com>
2026-05-11 21:30:55 +0100

    Modificar la ruta principal para que devuelva un JSON en lugar de una vista

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () { // Ruta principal
-    return view('welcome');
+Route::get('/', function (): JsonResponse { // Ruta principal
+    return response()->json([
+        'message' => 'API de evaluacion infantil activa',
+    ]);
 });
 
 require __DIR__.'/auth.php'; // Importamos las rutas de autenticación (registro, login, etc)
